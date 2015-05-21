46a289dabf7257ff8795f4a08903969fbecca44a
Diegohdz <somerdiego@hotmail.com>
2015-05-20 19:56:34 -0500

    Update DDNewton.java

--- DDNewton.java
+++ DDNewton.java
@@ -10,21 +10,37 @@
  */
 public class DDNewton 
 {
+  /**
+   * atributo de tipo entero para n 
+   */
   int n;
   double [] X = new double[4];
   double [] Y = new double[4];
     
+  /**
+   * método que asigna valor a atributo n
+   * @param N atributo de tipo entero
+   */
   public DDNewton(int N)
   {
     this.n = N; 
   }
   
+   /**
+   * método que asigna valor a atributo X y Y
+   * @param X atributo de tipo double
+   * @param Y atributo de tipo double
+   */
   public void setXY(double[] X, double[] Y)
   {
     this.X = X;
     this.Y = Y;
   }    
 
+  /**
+   * método
+   * @return devuelve polinomio de tipo double 
+   */
   public double[] getPolinomio()
   {
     double[] polinomio = {0.0, 0.0};
