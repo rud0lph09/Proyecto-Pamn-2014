5fae3ca322007e2f4adfa80f979be912d85a144d
Diegohdz <somerdiego@hotmail.com>
2015-05-20 20:03:36 -0500

    Update Gauss.java

--- Gauss.java
+++ Gauss.java
@@ -11,31 +11,45 @@
  */
 public class Gauss 
 {
+    /**
+	 * atributo de tipo entero para n y double para cte y matrices de tipo double
+	 */
     int n;
     public double cte;
     public double [][] A = new double [4][4];
     public double [] B = new double [4];
 
-
+     /**
+     * método que asigna valor a atributo n
+     * @param n atributo de tipo entero
+     */
     public Gauss(int n)
     {
         this.n = n;
     }
 
-    
-
+    /**
+     * método
+     * @param Aa Se asigna la matriz A a Aa
+     */
     public void setA(double[][] Aa)
     {
         double [][] A = Aa;
 
     }
-
+    
+     /**
+     * método
+     * @param Bb Se asigna la matriz B a Ba
+     */
     public void setB(double[] Bb)
     {
         double [] B = Bb;
     }
     
-
+     /**
+     * Se realizan ciclos
+     */
     public void ciclos(){
 
         for (int i = 0; i < n; ++i)
@@ -54,7 +68,11 @@ public class Gauss
 
         }
     }
-
+    
+    /**
+     * método 
+     * @return devuelve valor de atributo B
+     */
     public double[] getX()
     {
         return B;
